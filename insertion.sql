d54991955b8586add39374c2e4e9027cdcb89860
manivarshith28 <manivarshith2806@gmail.com>
2025-06-25 21:00:34 +0530

    Insertion operations on "cricketers" table

---
+++ insertion.sql
@@ -0,0 +1,48 @@
+-- Inserting only one record
+
+insert into cricketers
+(cricketer_id, name, jersy_no, country, gender)
+values
+(1, 'dhoni', 7, 'india', 'male');
+
+-- Inserting multiple records
+
+insert into cricketers
+(cricketer_id, name, jersy_no, country, gender) 
+values (2, 'kohli', 18, 'india', 'male'), 
+(3, 'williamson', null, 'new zealand', 'male'), 
+(4, 'smith', null, 'australia', 'male'), 
+(5, 'root', null, 'england', 'male'), 
+(6, 'mithali raj', null, 'india', 'female'), 
+(7, 'harmanpreet kaur', 7, 'india', 'female'), 
+(8, 'smriti mandhana', 18, 'india', 'female'), 
+(9, 'k l rahul', 1, 'india', 'male'), 
+(10, 'ben stokes', null, 'england', 'male');
+
+insert into cricketers
+(cricketer_id, name, jersy_no, country, gender)
+values
+(11, 'rohit sharma', '45', 'india', 'male'),
+(12, 'ricky ponting', null, 'australia', 'male'),
+(13, 'meg lanning', null, 'australia', 'female'),
+(14, 'ellyse perry', null, 'australia', 'female'),
+(15, 'sophie devine', null, 'new zealand', 'female'),
+(16, 'chris gayle', null, 'west indies', 'male'),
+(17, 'ab de villiers', 17, 'south africa', 'male'),
+(18, 'kagiso rabada', null, 'south africa', 'male'),
+(19, 'faf du plessis', null, 'south africa', 'male'),
+(20, 'sachin tendulkar', 10, 'india', 'male');
+
+insert into cricketers 
+(cricketer_id, name, jersy_no, country, gender) 
+values 
+(21, 'healy matthews', null, 'west indies', 'female'), 
+(22, 'jasprit bumrah', 93, 'india', 'male');
+
+insert into cricketers 
+(cricketer_id, name, jersy_no, country, gender) 
+values 
+(23, 'sangakkara', null, 'sri lanka', 'male'), 
+(24, 'shami', null, 'india', 'male'),  
+(25, 'ashwin', 99, 'india', 'male');
+
